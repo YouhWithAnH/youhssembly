453dfad0928a09c81382fc5b91aafee6db6e069e
youhwithanh <lipegrs+5@gmail.com>
2026-02-09 14:52:20 -0300

    removed info present on the wiki about code

--- youhssembly.py
+++ youhssembly.py
@@ -6,30 +6,6 @@ def clamp(n: int,min_v,max_v):
 alloc = [0]
 mem_tape = [0] * 4096
 def YSM(mem: list, acc: list,code: str):
-    """
-    Runs Youhssembly code through a string. Youhssembly is a limited version of assembly made as a test
-    programming language for simulating CPU and RAM relations using Python. Below is a list of every function.
-    ```python
-    REG(addr,value) -> bin # Registers a given integer value as binary in memory.
-    MOV(addr1,addr2) -> bin # Copies the data from a given address to another.
-    LDA(value) -> bin # Loads an integer value into the accumulator. The accumulator can be defined through a parameter in the function.
-    STA(addr) -> bin # Stores the accumulator's value into an address.
-    ADD(addr,value) -> None # Adds an integer to a given address.
-    SUB(addr,value) -> None # Subtracts a given address's value by an integer.
-    MLT(addr,value) -> None # Multiplies an integer value by the address's value.
-    DIV(addr,value) -> None # Divides an integer value by the address's value. Division is rounded DOWN.
-    SAY(addr) -> bin # Prints out a byte's value.
-    ECHO(any) -> str # Echoes any value.
-    CHR(addr) -> chr # Prints out a byte's value.
-    WHILE(addr, kw, val) -> None # Perform a while loop for any actions while an addresses obeys a keyword-value statement, such as '<addr> > 0'.
-    WHILEND(addr) -> None # End a while loop that utilizes the given address.
-
-    ``` 
-    
-    :param mem: memory tape. Type must be list.
-    :param acc: accumulator byte. Type must be list.
-    :param code: code. Type must be string.
-    """
     ########
     # const
     ########
@@ -92,12 +68,10 @@ def YSM(mem: list, acc: list,code: str):
                             if param[1:] in digits:
                                 param = int(param)
             if line_inst == "REG":
-                # REG <addr> <value> | :param addr -- 0 <= <addr> < len(tape) | :param value -- 0 <= <value> <= int_limit
                 if len(line_params) == 2 and 0 <= int(line_params[0]) <= len(mem) and 0 <= int(line_params[1]) <= int_limit:
                     mem[int(line_params[0])] = int(line_params[1])
             
             if line_inst == "MOV":
-                # MOV <addr_sender> <addr_receiver> | mem[addr_receiver] = mem[addr_sender]
                 if len(line_params) == 2 and 0 <= int(line_params[0]) <= len(mem) and 0 <= int(line_params[1]) <= len(mem):
                     mem[int(line_params[1])] = mem[int(line_params[0])]
             
